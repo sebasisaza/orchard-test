c7c481398553c0a2b306ba5b563ae67a7c363124
Sebastian Isaza <sebastian.isaza@nearsure.com>
2025-06-06 11:29:43 -0500

    Get everything done & responsive

--- orchard/src/app/app.ts
+++ orchard/src/app/app.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
-export class App {
+export class App implements OnInit {
   protected title = 'orchard';
+
+  constructor(private renderer: Renderer2) {}
+
+  ngOnInit() {
+    this.renderer.listen('document', 'click', (event: MouseEvent) => {
+      const anchor = (event.target as HTMLElement).closest('a');
+      if (anchor) {
+        console.log('Anchor clicked:', anchor);
+      }
+    });
+  }
 }
